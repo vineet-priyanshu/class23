e46eac0e59c4a7b1cf182edfd2d9a336b47463fa
VINEET PRIYANSHU <74959291+vineet-priyanshu@users.noreply.github.com>
2020-12-04 15:57:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class23
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,46 @@
+var bullet,wall;
+var speed,weight,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+  thickness=random(22,83)
+  speed=random(52,90);
+  weight=random(30,52);
+
+  bullet=createSprite(50, 200, 50, 20);
+  bullet.shapeColor="black";
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+
+  
+  bullet.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);  
+ if (hasCollided(bullet,wall)){
+   bullet.velocityX=0;
+   var damage=0.5 * weight * speed * speed/(thickness*thickness*thickness);
+  }
+   if(damage>10){
+    wall.shapeColor=color(255,0,0);
+ }
+ 
+ if(damage<10){
+   wall.shapeColor=color(0,255,0)
+ }
+
+  drawSprites();
+}
+
+function hasCollided(b,w){
+  bulletRightEdge= b.x + b.width;
+  wallLeftEdge=w.x;
+  if (bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false
+  
+}
